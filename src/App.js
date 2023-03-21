6ce3834be1ebe477d2a29de019af15536fabaaac
monika <monika772408@gmail.com>
2023-03-22 00:26:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Form } from "./Components/Form";
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h2> TO DO LIST
+      </h2>
+      <div className="form-and-todo-box">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
